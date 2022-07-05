b2ddb75a24d479f1415df9b72eafbeebdf5dc2ba
svyatx-code <108305142+svyatx-code@users.noreply.github.com>
2022-07-06 03:18:59 +0500

    Scroll Physics live matter!

--- lib/screens/chatDetailPage.dart
+++ lib/screens/chatDetailPage.dart
@@ -84,7 +84,7 @@ class _ChatDetailPageState extends State<ChatDetailPage> {
               itemCount: messages.length,
               shrinkWrap: true,
               padding: const EdgeInsets.only(top: 10, bottom: 10),
-              physics: const NeverScrollableScrollPhysics(),
+              physics: const BouncingScrollPhysics(),
               itemBuilder: (context, index) {
                 return Container(
                   padding: const EdgeInsets.only(
